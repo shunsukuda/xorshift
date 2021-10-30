0108feed1e986457908d7618ee42ad12f2019e66
Shunsuke Tsukuda <shunsukuda@gmail.com>
2021-10-30 19:04:28 +0900

    Add Xoroshiro256p(+)

---
+++ xoroshiro256p.go
@@ -0,0 +1,89 @@
+package xrand
+
+import "math/bits"
+
+// https://prng.di.unimi.it/xoroshiro256plus.c
+
+type Xoroshiro256p struct {
+	s [4]uint64
+}
+
+func (x *Xoroshiro256p) Seed(seed int64) {
+	s := NewSplitMix64(seed)
+	x.s[0] = s.Uint64()
+	x.s[1] = s.Uint64()
+	x.s[2] = s.Uint64()
+	x.s[3] = s.Uint64()
+}
+
+func (x *Xoroshiro256p) Uint64() uint64 {
+	result := x.s[0] + x.s[3]
+	t := x.s[1] << 17
+	x.s[2] ^= x.s[0]
+	x.s[3] ^= x.s[1]
+	x.s[1] ^= x.s[2]
+	x.s[0] ^= x.s[3]
+
+	x.s[2] ^= t
+	x.s[3] = bits.RotateLeft64(x.s[3], 45)
+
+	return result
+}
+
+func (x *Xoroshiro256p) Int64() int64 {
+	return unsafeUint64ToInt64(x.Uint64())
+}
+
+func (x *Xoroshiro256p) Int63() int64 {
+	return int64(x.Uint64() & (1<<63 - 1))
+}
+
+// Call Uint64() * 2^128
+func (x *Xoroshiro256p) Jump() {
+	var (
+		jump = [...]uint64{0x180ec6d33cfd0aba, 0xd5a61266f0c9392c, 0xa9582618e03fc9aa, 0x39abdc4529b1661c}
+	)
+
+	s := [...]uint64{0, 0, 0, 0}
+	for i := range jump {
+		for b := 0; b < 64; b++ {
+			if (jump[i] & 1 << b) != 0 {
+				s[0] ^= x.s[0]
+				s[1] ^= x.s[1]
+				s[2] ^= x.s[2]
+				s[3] ^= x.s[3]
+			}
+		}
+		x.Uint64()
+	}
+
+	x.s[0] = s[0]
+	x.s[1] = s[1]
+	x.s[2] = s[2]
+	x.s[3] = s[3]
+}
+
+// Call Uint64() * 2^192
+func (x *Xoroshiro256p) LongJump() {
+	var (
+		jump = [...]uint64{0x76e15d3efefdcbbf, 0xc5004e441c522fb3, 0x77710069854ee241, 0x39109bb02acbe635}
+	)
+
+	s := [...]uint64{0, 0, 0, 0}
+	for i := range jump {
+		for b := 0; b < 64; b++ {
+			if (jump[i] & 1 << b) != 0 {
+				s[0] ^= x.s[0]
+				s[1] ^= x.s[1]
+				s[2] ^= x.s[2]
+				s[3] ^= x.s[3]
+			}
+		}
+		x.Uint64()
+	}
+
+	x.s[0] = s[0]
+	x.s[1] = s[1]
+	x.s[2] = s[2]
+	x.s[3] = s[3]
+}
